4dbc3d411505be14491a43d12f8731ad52d0ad00
Tony-ru17 <tonyroses2005@gmail.com>
2025-04-04 09:59:30 +0200

    refactor SQL scripts for improved readability and formatting

--- ejercicio5_3.sql
+++ ejercicio5_3.sql
@@ -13,8 +13,8 @@ que si el curso no tiene tutor, también deberá salir, ordena por curso.*/
 
 SELECT curso.ABREVIATURA, COUNT(*) AS 'Número de Alumnos', modulo.profesor AS 'Tutor'
 FROM CICLOS_CURSO curso
-JOIN CICLOS_ALUMNO alumno ON alumno.CICLO = curso.ABREVIATURA
-LEFT JOIN CICLOS_MODULO modulo ON curso.ABREVIATURA = modulo.curso AND modulo.nombre = 'Tutoría'
+         JOIN CICLOS_ALUMNO alumno ON alumno.CICLO = curso.ABREVIATURA
+         LEFT JOIN CICLOS_MODULO modulo ON curso.ABREVIATURA = modulo.curso AND modulo.nombre = 'Tutoría'
 GROUP BY curso.ABREVIATURA, modulo.profesor
 ORDER BY curso.ABREVIATURA;
 
@@ -25,20 +25,17 @@ tiene.*/
 
 SELECT departamento.NOMBRE, departamento.JEFE, COUNT(*) 'Contador'
 FROM CICLOS_DEPARTAMENTO departamento
-JOIN CICLOS_PROFESORES profesores ON departamento.NOMBRE=profesores.DEPARTAMENTO
+         JOIN CICLOS_PROFESORES profesores ON departamento.NOMBRE=profesores.DEPARTAMENTO
 GROUP BY departamento.NOMBRE,departamento.JEFE;
-
-/*4. Muestra las notas del periodo 1 que faltan por introducir por alumno y
-
 
 /*4. Muestra las notas del periodo 1 que faltan por introducir por alumno y
 módulo.*/
 
 SELECT CONCAT(alumno.NOMBRE, ' ', alumno.APELLIDOS) AS Alumno, modulo.nombre AS Modulo
 FROM CICLOS_ALUMNO alumno
-JOIN CICLOS_CURSO curso ON alumno.CICLO = curso.ABREVIATURA
-JOIN CICLOS_MODULO modulo ON curso.ABREVIATURA = modulo.curso
-LEFT JOIN CICLOS_EVALUACION notas ON alumno.NIA = notas.ALUMNO AND modulo.codigo = notas.MODULO AND notas.EVALUACION = 1
+         JOIN CICLOS_CURSO curso ON alumno.CICLO = curso.ABREVIATURA
+         JOIN CICLOS_MODULO modulo ON curso.ABREVIATURA = modulo.curso
+         LEFT JOIN CICLOS_EVALUACION notas ON alumno.NIA = notas.ALUMNO AND modulo.codigo = notas.MODULO AND notas.EVALUACION = 1
 WHERE notas.NOTA IS NULL
 ORDER BY CONCAT(alumno.NOMBRE, ' ', alumno.APELLIDOS);
 
@@ -50,31 +47,43 @@ ORDER BY CONCAT(alumno.NOMBRE, ' ', alumno.APELLIDOS);
 no hayan vendido ningún coche, ordenados por número de ventas de
 forma descendente y por nombre de concesionario.*/
 
-SELECT SUM(distribucion.CANTIDAD) 'Total coches vendidos', conce.NOMBRE AS CONCESIONARIO
-FROM conce_distribucion distribucion
-JOIN conce_concesionario conce ON distribucion.CONCESIONARIO=conce.CIF
-GROUP BY distribucion.CONCESIONARIO
+
+SELECT COUNT(*) 'Total coches vendidos', conce.NOMBRE AS CONCESIONARIO
+FROM CONCE_VENTAS ventas
+         RIGHT JOIN CONCE_CONCESIONARIO conce ON ventas.CONCESIONARIO=conce.CIF
+GROUP BY conce.CIF
 ORDER BY `Total coches vendidos` DESC;
-
-
 
 /*2. Visualiza el número de coches vendidos, incluso para aquellos coches
 que no hayan sido vendidos nunca ordenado descendentemente por el
 total de coches y por el coche.*/
 
 SELECT SUM(distribucion.CANTIDAD) 'Total coches vendidos', coche.NOMBRE AS Coche
-FROM conce_distribucion distribucion
-JOIN conce_coche coche ON distribucion.COCHE=coche.CODIGO
-GROUP BY distribucion.COCHE
+FROM CONCE_DISTRIBUCION distribucion
+         RIGHT JOIN CONCE_COCHE coche ON distribucion.COCHE=coche.CODIGO
+GROUP BY coche.CODIGO
 ORDER BY `Total coches vendidos` DESC;
 
 /*3. Queremos saber el stock total de cada concesionario (es decir, la
 cantidad total de coches distribuidos), incluso aquellos que no tengan
 Stock ordenado de mayor a menor stock.*/
 
+SELECT SUM(distribucion.CANTIDAD) 'Stock', conce.NOMBRE AS CONCESIONARIO
+FROM CONCE_DISTRIBUCION distribucion
+         RIGHT JOIN CONCE_CONCESIONARIO conce ON distribucion.CONCESIONARIO=conce.CIF
+GROUP BY conce.NOMBRE
+ORDER BY `Stock` ASC;
+
 /*4.De la consulta anterior:
     a. Filtrar aquellos concesionarios que no tengan stock
     b. Filtrar aquellos concesionarios cuyo stock sea mayor o igual a 20.*/
+SELECT SUM(distribucion.CANTIDAD) 'Stock', conce.NOMBRE AS CONCESIONARIO
+FROM CONCE_DISTRIBUCION distribucion
+         RIGHT JOIN CONCE_CONCESIONARIO conce ON distribucion.CONCESIONARIO=conce.CIF
+GROUP BY conce.NOMBRE
+HAVING Stock IS NULL
+ORDER BY `Stock` ASC;
+
 
 -- ----------LISTAS---------------
 /*1.Obtén el número de artistas, ordenados por número de artistas de
